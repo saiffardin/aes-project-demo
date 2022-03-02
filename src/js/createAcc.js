9563f5c88412ce5295801c71af163c202af375b0
saiffardin <saiffardin@gmail.com>
2022-03-02 14:52:18 +0600

    role || select menu

--- src/js/createAcc.js
+++ src/js/createAcc.js
@@ -1,1 +1,8 @@
 console.log('createAcc');
+
+const {ipcRenderer} = require('electron');
+
+const inputEmpId = document.getElementById('ele-emp-id');
+const inputEmpPassword = document.getElementById('ele-emp-password');
+const formLogin = document.getElementById('form-login');
+const createAccountSpan = document.getElementById('span-create-acc');
